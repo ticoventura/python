1d0cd8b7d5766d2c816c8443b4ccce6b16023186
ticoventura <42718816+ticoventura@users.noreply.github.com>
2020-02-26 20:15:59 -0300

    Delete lista_4.py

--- lista_4.py
+++
@@ -1,8 +0,0 @@
-#4) Leia 2 valores de ponto flutuante A e B, que correspondem a 2 notas de um aluno. 
-#A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5.
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-
-media = (n1*3.5 + n2*7.5)/11
-
-print(f"A média é: {media}")
