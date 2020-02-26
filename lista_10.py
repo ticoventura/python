c139a34c8e9c9694b2a20609598f93eae699ab98
ticoventura <42718816+ticoventura@users.noreply.github.com>
2020-02-26 20:15:27 -0300

    Delete lista_10.py

--- lista_10.py
+++
@@ -1,7 +0,0 @@
-#10) Faça um programa que leia a quantidade de dias, horas, minutos e segundos. Calcule o total em segundos.
-d = int(input("Digite a quantidade de dias: "))
-h = int(input("Digite a quantidade de horas: "))
-m = int(input("Digite a quantidade de minutos: "))
-s = int(input("Digite a quantidade de segundos: "))
-
-print(f"O total, em segundos é: {s + m*60 + h*3600 + d*86400}")
