eafc33b7ad48980f96b7dca6c3cb23b5422d2839
ticoventura <42718816+ticoventura@users.noreply.github.com>
2020-02-26 20:16:21 -0300

    Delete lista_6.py

--- lista_6.py
+++
@@ -1,6 +0,0 @@
-#6) Faça um programa que leia um valor em metros e o mostre convertido em milímetros.
-m = float(input("Digite o valor em metros: "))
-
-mm = m*1000
-
-print(f"O valor digitado, em milímetros é: {mm}")
